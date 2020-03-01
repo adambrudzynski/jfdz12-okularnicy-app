e8d017c99caf5ec40dc46627d90ee6341b0e7203
Adam Brudzynski <adam.brudzynski@gmail.com>
2020-03-01 14:32:33 +0100

    Updates

---
+++ precache-manifest.eb0f5cbbb7965caf408f36ad80eeb2db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef8dd88b24b272d5998d7160d2870fa8",
+    "url": "/jfdz12-okularnicy-app/index.html"
+  },
+  {
+    "revision": "c641517e36682928b538",
+    "url": "/jfdz12-okularnicy-app/static/css/main.0c47d992.chunk.css"
+  },
+  {
+    "revision": "8546ec0881202c400b16",
+    "url": "/jfdz12-okularnicy-app/static/js/2.ac0b814d.chunk.js"
+  },
+  {
+    "revision": "d6d5edf8da0429c8f750691ce51d6907",
+    "url": "/jfdz12-okularnicy-app/static/js/2.ac0b814d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c641517e36682928b538",
+    "url": "/jfdz12-okularnicy-app/static/js/main.186c7feb.chunk.js"
+  },
+  {
+    "revision": "300e24cb6514b48a630d",
+    "url": "/jfdz12-okularnicy-app/static/js/runtime-main.80f2dc9c.js"
+  },
+  {
+    "revision": "2043bcd76e02f41ca18268e1fde96840",
+    "url": "/jfdz12-okularnicy-app/static/media/background.2043bcd7.jpg"
+  }
+]);
