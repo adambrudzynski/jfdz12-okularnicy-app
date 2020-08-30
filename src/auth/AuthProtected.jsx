d5bb5c3836b3ffbaa9994e67d072fd1c9d4374c9
Adam <adam.brudzynski@gmail.com>
2020-08-30 19:58:59 +0200

    auth watching fix

--- src/auth/AuthProtected.jsx
+++ src/auth/AuthProtected.jsx
@@ -1,12 +1,53 @@
-import React, { useContext } from 'react'
-import { MyContext } from './Auth'
+import React, { useContext, useEffect, useState } from 'react'
+import { Segment, Loader, Dimmer, Placeholder } from 'semantic-ui-react';
+import firebase from "firebase";
 import { Login } from './LogIn'
+
+
+
 
  const AuthProtected = (props) => {
 
-    const context = useContext(MyContext)
+    const [user, setUser] = useState(false)
+     useEffect(() => {
+         auth()
+        //  return () => {
+            //  cleanup
+        //  }
+     }, [])
 
-    return context.state.user ? { ...props.children } : <Login />
+     const loading =    <> 
+                    <Segment>
+                    <Placeholder fluid>             
+                        <Placeholder.Line length='full' />
+                        <Placeholder.Line length='medium' />
+                    </Placeholder>
+                    </Segment>
+                  <Segment>    
+                    <Placeholder fluid>             
+                        <Placeholder.Line length='full' />
+                        <Placeholder.Line length='full' />
+                        <Placeholder.Line length='full' />
+                        <Placeholder.Line length='full' />
+                        <Placeholder.Line length='full' />
+                    </Placeholder>
+                </Segment>
+                </> 
+
+
+    const auth = () => firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          setUser(user) 
+        } else {
+           setUser(null)
+        }
+      });
+      return <>
+        {user === false &&  loading}
+        {user === null && <Login/>}
+        {user && {...props.children}}
+        </>
+    
 }
 
 export default AuthProtected
